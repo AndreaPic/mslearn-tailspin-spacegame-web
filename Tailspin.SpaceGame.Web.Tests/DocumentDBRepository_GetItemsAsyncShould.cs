a2b824b69a2e7f2b636abfd88e5dfa631c81de40
Andrea Piccioni <andrea.piccioni@libero.it>
2021-08-18 11:05:04 +0200

    Run and publish unit tests

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -29,7 +29,8 @@ namespace Tests
         [TestCase("Pinwheel")]
         [TestCase("NGC 1300")]
         [TestCase("Messier 82")]
-        public void FetchOnlyRequestedGameRegion(string gameRegion)
+        //public void FetchOnlyRequestedGameRegion(string gameRegion)
+        public async Task FetchOnlyRequestedGameRegion(string gameRegion)
         {
             const int PAGE = 0; // take the first page of results
             const int MAX_RESULTS = 10; // sample up to 10 results
@@ -39,14 +40,20 @@ namespace Tests
             Expression<Func<Score, bool>> queryPredicate = score => (score.GameRegion == gameRegion);
 
             // Fetch the scores.
-            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
+            // Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
+            //     queryPredicate, // the predicate defined above
+            //     score => 1, // we don't care about the order
+            //     PAGE,
+            //     MAX_RESULTS
+            // );
+            //IEnumerable<Score> scores = scoresTask.Result;
+            IEnumerable<Score> scores = await _scoreRepository.GetItemsAsync(
                 queryPredicate, // the predicate defined above
                 score => 1, // we don't care about the order
                 PAGE,
                 MAX_RESULTS
             );
-            IEnumerable<Score> scores = scoresTask.Result;
-
+            
             // Verify that each score's game region matches the provided game region.
             Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
         }
